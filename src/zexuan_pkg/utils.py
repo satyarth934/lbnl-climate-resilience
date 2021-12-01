d5d5ef0b78964bd28dce3ed668e10ac1ce7fafd7
Satyarth Praveen <satyarth@lbl.gov>
2021-11-30 17:16:52 -0800

    Added some utility code. 1. Added a context manager to be used with the 'with' clause to calcule the execution time of a block of code. 2. Added a format to print the warnings.

--- src/zexuan_pkg/utils.py
+++ src/zexuan_pkg/utils.py
@@ -1,5 +1,6 @@
 import sys
 import yaml
+import time
 
 
 def parse_input_yaml(yaml_path: str) -> dict:
@@ -22,7 +23,46 @@ def stall():
     continue to the next iteration or quit the loop.
     """
     
-    inp = input("Enter to continue...")
+    inp = input("Press [q|Q] to quit OR Enter to continue... ")
     
     if inp.lower() == 'q':
         sys.exit(0)
+
+    
+class ExecTimeCM(object):
+    """Context manager to be used with the 'with' clause 
+    to get the execution time of a code block.
+    
+    Example Usage:
+        with ExecTimeCM() as st:
+            print("sample code block.")
+    """
+    
+    def __init__(self, txt="", verbose=True):
+        self.txt = txt
+        self.verbose = verbose
+        self.description = f"EXECUTION TIME for {self.txt}"
+    
+    def __enter__(self):
+        self.start = time.time()
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.exec_time = time.time() - self.start
+        if self.verbose:
+            print(f"{self.description}: {self.exec_time} seconds")
+
+        
+def warning_format(msg, *args, **kwargs):
+    """Used to generate warnings within code.
+    Prints the warning in the following format:
+    <script>:<n>: <type of warning>: <warning msg>
+    
+    'n' is the line number of initiated warning command within the script
+    
+    Example Usage:
+        warnings.formatwarning = warning_format
+        warnings.warn("<Warning msg>")
+    """
+    
+    return f"{args[1]}:{args[2]}: {args[0].__name__}: {str(msg)}\n"
